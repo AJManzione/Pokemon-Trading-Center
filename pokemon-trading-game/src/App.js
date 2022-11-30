5dee7b5917edcb8c24ac00a9b90e6283a983714d
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-11-30 16:48:36 -0700

    added base files

---
+++ pokemon-trading-game/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pokemon from './components/Pokemon';
+
+function App() {
+  return <Pokemon />;
+}
+
+export default App;
---
+++ pokemon-trading-game/src/components/Pokemon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Pokemon() {
+  
+  return (
+    <div className='card'>
+        <div className='card-header'>
+            <h1>Hello World!</h1>
+        </div>
+        <div className='card-body'>
+            <p>My name is Anthony!</p>
+        </div>
+    </div>
+  );
+}
+
+export default Pokemon;
