d9362f78982c870fc8b92ab7ad16788ce3eaf2cc
michael seaman <mseaman26@gmail.com>
2022-12-04 20:31:35 -0800

    getting ready for push

--- pokedex-tracker/server/schemas/typeDefs.js
+++ pokedex-tracker/server/schemas/typeDefs.js
@@ -1,13 +1,13 @@
 const { gql } = require('apollo-server-express');
 
-
+//TODO: add pokemonCaught
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     createdAt: String
     sprite: String
-
+    
   }
 
   type Query {
