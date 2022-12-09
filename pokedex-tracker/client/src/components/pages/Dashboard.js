c04f37997471af85297321410822c811d27e032c
michael seaman <mseaman26@gmail.com>
2022-12-08 21:55:55 -0800

    Router Refactor

--- pokedex-tracker/client/src/components/pages/Dashboard.js
+++ pokedex-tracker/client/src/components/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import UserMenu from '../UserMenu';
-import BadgeKey from '../BadgeKey'
 import '../../styles/dashboard.css';
 import pokeball from '../../images/badges/pokeball-badge.png'
 
@@ -59,88 +58,3 @@ export default function Dashboard() {
   );
 }
 
-/* <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Johto</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Hoenn</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Sinnoh</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Unova</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Kalos</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Alola</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Galar</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul>
-              </li>
-              <li className="list-group-item-success m-1">
-                <ul className='d-flex justify-content-between align-items-center'>
-                  <div className='d-flex align-items-center justify-content-around'> 
-                    <li>
-                      <h1>Paldea</h1>
-                      <br></br>
-                    </li>
-                  </div>  
-                </ul> */
