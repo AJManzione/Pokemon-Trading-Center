d4fbd2432953957676bee6f94e48056e82b56174
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-12-12 14:33:46 -0700

    all types rendered, needs styling

--- pokedex-tracker/client/src/components/pages/GenTwo.js
+++ pokedex-tracker/client/src/components/pages/GenTwo.js
@@ -1,137 +1,119 @@
-import React, { useState, useEffect, useRef } from 'react';
-import pokeballOpen from '../../images/pokeball-open.png'
-import pokeballClosed from '../../images/pokeball-closed.png'
-import Tada from 'react-reveal/Tada'
-import { useMutation, useQuery } from '@apollo/client';
-import { CATCH_POKEMON, UNCATCH_POKEMON } from '../../utils/mutations';
-import { QUERY_USER } from '../../utils/queries';
-import bug from '../../images/types/bug.png';
-import dark from '../../images/types/dark.png';
-import dragon from '../../images/types/dragon.png';
-import electric from '../../images/types/electric.png';
-import fairy from '../../images/types/fairy.png';
-import fire from '../../images/types/fire.png';
-import flying from '../../images/types/flying.png';
-import fighting from '../../images/types/fighting.png'
-import ghost from '../../images/types/ghost.png';
-import grass from '../../images/types/grass.png';
-import ground from '../../images/types/ground.png';
-import ice from '../../images/types/ice.png';
-import normal from '../../images/types/normal.png';
-import poison from '../../images/types/poison.png';
-import psychic from '../../images/types/psychic.png';
-import rock from '../../images/types/rock.png';
-import steel from '../../images/types/steel.png';
-import water from '../../images/types/water.png'; 
+import React, { useState, useEffect, useRef } from "react";
+import pokeballOpen from "../../images/pokeball-open.png";
+import pokeballClosed from "../../images/pokeball-closed.png";
+import Tada from "react-reveal/Tada";
+import { useMutation, useQuery } from "@apollo/client";
+import { CATCH_POKEMON, UNCATCH_POKEMON } from "../../utils/mutations";
+import { QUERY_USER } from "../../utils/queries";
+import bug from "../../images/types/bug.png";
+import dark from "../../images/types/dark.png";
+import dragon from "../../images/types/dragon.png";
+import electric from "../../images/types/electric.png";
+import fairy from "../../images/types/fairy.png";
+import fire from "../../images/types/fire.png";
+import flying from "../../images/types/flying.png";
+import fighting from "../../images/types/fighting.png";
+import ghost from "../../images/types/ghost.png";
+import grass from "../../images/types/grass.png";
+import ground from "../../images/types/ground.png";
+import ice from "../../images/types/ice.png";
+import normal from "../../images/types/normal.png";
+import poison from "../../images/types/poison.png";
+import psychic from "../../images/types/psychic.png";
+import rock from "../../images/types/rock.png";
+import steel from "../../images/types/steel.png";
+import water from "../../images/types/water.png";
 
+export default function GenTwo() {
+  // fetching all pokemon from JSON Data
+  const [JSONdata, setJSONData] = useState([]);
 
-export default function  GenTwo() {
-
-// fetching all pokemon from JSON Data
-const [JSONdata, setJSONData] = useState([]);
-
-const getData=()=>{
-  fetch('./gen-2.json'
-  ,{
-    headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }
-  }
-  )
-    .then(function(response){
-      return response.json();
+  const getData = () => {
+    fetch("./gen-2.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-    .then(function(myJson) {
-      setJSONData(myJson)
-    })
-}
-useEffect(()=>{
-  getData()
-},[])
-// end of fetch
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setJSONData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  // end of fetch
 
+  // username
+  const currentUser = localStorage.getItem("username");
 
-// username
-const currentUser = localStorage.getItem('username')
+  // queries and mutations
+  const [catchPokemon, { catchErr, catchData }] = useMutation(CATCH_POKEMON);
+  const [unCatchPokemon, { unCatchErr, unCatchData }] =
+    useMutation(UNCATCH_POKEMON);
 
-
-// queries and mutations
-const [catchPokemon, { catchErr, catchData }] = useMutation(CATCH_POKEMON);
-const [unCatchPokemon, { unCatchErr, unCatchData }] = useMutation(UNCATCH_POKEMON);
-
-const { loading, data: userValue } = useQuery(QUERY_USER, {
-  variables: { username: currentUser },
-  
-});
-
-
-// users pokemon caught state
-const [userData, setUserData] = useState({
-  pokemonCaught: [],
-});
-
-
-//  setting the state of pokemon caught
-useEffect(() => {
-  if (!loading && userValue ) {
-    setUserData({ 
-      pokemonCaught: userValue?.user?.pokemonCaught?.map((caught) => caught.entry)
-    });
-  }
-}, [ loading, userValue ]);
- 
-
-function newCatch(entry) {
-
-  catchPokemon({
-    variables: {
-      username: currentUser,
-      entry: parseFloat(entry)
-    }
+  const { loading, data: userValue } = useQuery(QUERY_USER, {
+    variables: { username: currentUser },
   });
 
-};
+  // users pokemon caught state
+  const [userData, setUserData] = useState({
+    pokemonCaught: [],
+  });
 
+  //  setting the state of pokemon caught
+  useEffect(() => {
+    if (!loading && userValue) {
+      setUserData({
+        pokemonCaught: userValue?.user?.pokemonCaught?.map(
+          (caught) => caught.entry
+        ),
+      });
+    }
+  }, [loading, userValue]);
 
-function releaseCatch(entry) {
+  function newCatch(entry) {
+    catchPokemon({
+      variables: {
+        username: currentUser,
+        entry: parseFloat(entry),
+      },
+    });
+  }
 
+  function releaseCatch(entry) {
     unCatchPokemon({
       variables: {
         username: currentUser,
-        entry: parseFloat(entry)
-      }
+        entry: parseFloat(entry),
+      },
     });
-
-
-};
-function toggleCatch(entry){
-  
-  if(!userData.pokemonCaught.includes(parseFloat(entry))){
-    newCatch(entry)
-    userData.pokemonCaught = [...userData.pokemonCaught, parseFloat(entry)]
-    document.getElementById(`${entry}`).src = pokeballClosed
-    console.log(userData.pokemonCaught)
- 
-  }else{
-    releaseCatch(entry)
-    userData.pokemonCaught = userData.pokemonCaught.filter(pokemon => pokemon != parseFloat(entry))
-    document.getElementById(`${entry}`).src = pokeballOpen
-    console.log(userData.pokemonCaught)
-   
   }
-}
 
-
+  function toggleCatch(entry) {
+    if (!userData.pokemonCaught.includes(parseFloat(entry))) {
+      newCatch(entry);
+      userData.pokemonCaught = [...userData.pokemonCaught, parseFloat(entry)];
+      document.getElementById(`${entry}`).src = pokeballClosed;
+      console.log(userData.pokemonCaught);
+    } else {
+      releaseCatch(entry);
+      userData.pokemonCaught = userData.pokemonCaught.filter(
+        (pokemon) => pokemon != parseFloat(entry)
+      );
+      document.getElementById(`${entry}`).src = pokeballOpen;
+      console.log(userData.pokemonCaught);
+    }
+  }
 
   const getTypeOne = (splitFirst) => {
-
-    let type = splitFirst.trim()
-    let split = type.split(" ")
-
+    let type = splitFirst.trim();
+    let split = type.split(" ");
 
     switch (split[0]) {
-
       case "Bug":
         return bug;
       case "Dark":
@@ -165,19 +147,17 @@ function toggleCatch(entry){
       case "Steel":
         return steel;
       case "Water":
-        return water; 
-        case "Grass": 
-        return grass     
+        return water;
+      case "Grass":
+        return grass;
     }
-  }
+  };
 
   const getTypeTwo = (splitSecond) => {
-  
-    let types = splitSecond.trim()
-    let split = types.split(" ")
+    let types = splitSecond.trim();
+    let split = types.split(" ");
 
     switch (split[1]) {
-
       case "Bug":
         return bug;
       case "Dark":
@@ -211,58 +191,66 @@ function toggleCatch(entry){
       case "Steel":
         return steel;
       case "Water":
-        return water; 
-      case "Grass": 
-        return grass   
+        return water;
+      case "Grass":
+        return grass;
     }
-  }
-
+  };
 
   return (
-    <div className='gen-two-bg'>
-      <div className='container pt-3'>
-        <ul className="list-group"> 
-        { JSONdata.map((pokemon, i) => {
-          return (  
-          <li className="list-group-item-success m-1" key={parseFloat(pokemon.entry)}>
-            <ul className='d-flex justify-content-between align-items-center'>
-              <div className='d-flex align-items-center justify-content-around'>
-                <li>
-                  <img 
-                    width='70px'
-                    src={`../../assets/sprites/Gen-2-Sprites/${parseFloat(pokemon.entry)}.png`}
-                    >
-                  </img>
-                </li>
-                <li className='m-4'>
-                  <h4>#{pokemon.entry}</h4>
-                </li>
-              </div>
-              <li><h4>{pokemon.name}</h4></li>
-              <li>
-                <img width='60px' src={getTypeOne(pokemon.type)}></img>
-                <img width='60px' src={getTypeTwo(pokemon.type)}></img>
-              </li> 
-              <li>
-                <Tada duration={2500}>       
-                <img
-                  id={parseFloat(parseFloat(pokemon.entry))}
-                  key={parseFloat(parseFloat(pokemon.entry))}
-                  className='pokeball'
-                  src={!userData.pokemonCaught.includes(parseFloat(pokemon.entry)) ?pokeballOpen : pokeballClosed}
-                  onClick={() => toggleCatch(parseFloat(pokemon.entry))}
-                  >
-                </img>
-                </Tada>
-              </li>  
-            </ul>    
-          </li>
-          )})}
+    <div className="gen-two-bg">
+      <div className="container pt-3">
+        <ul className="list-group">
+          {JSONdata.map((pokemon, i) => {
+            return (
+              <li
+                className="list-group-item-success m-1"
+                key={parseFloat(pokemon.entry)}
+              >
+                <ul className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center justify-content-around">
+                    <li>
+                      <img
+                        width="70px"
+                        src={`../../assets/sprites/Gen-2-Sprites/${parseFloat(
+                          pokemon.entry
+                        )}.png`}
+                      ></img>
+                    </li>
+                    <li className="m-4">
+                      <h4>#{pokemon.entry}</h4>
+                    </li>
+                  </div>
+                  <li>
+                    <h4>{pokemon.name}</h4>
+                  </li>
+                  <li>
+                    <img width="50px" src={getTypeOne(pokemon.type)}></img>
+                    <img width="50px" src={getTypeTwo(pokemon.type)}></img>
+                  </li>
+                  <li>
+                    <Tada duration={2500}>
+                      <img
+                        id={parseFloat(parseFloat(pokemon.entry))}
+                        key={parseFloat(parseFloat(pokemon.entry))}
+                        className="pokeball"
+                        src={
+                          !userData.pokemonCaught.includes(
+                            parseFloat(pokemon.entry)
+                          )
+                            ? pokeballOpen
+                            : pokeballClosed
+                        }
+                        onClick={() => toggleCatch(parseFloat(pokemon.entry))}
+                      ></img>
+                    </Tada>
+                  </li>
+                </ul>
+              </li>
+            );
+          })}
         </ul>
-      </div>  
+      </div>
     </div>
   );
 }
-
-
-
