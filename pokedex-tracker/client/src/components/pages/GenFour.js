5d3989e27a5049719fffdf25f30df581f26f582b
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-12-11 23:09:27 -0700

    working catch release functionality, except for floating point numbers(back-end-issue)

--- pokedex-tracker/client/src/components/pages/GenFour.js
+++ pokedex-tracker/client/src/components/pages/GenFour.js
@@ -1,26 +1,16 @@
-const pokemonCaughtArray = []
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import pokeballOpen from '../../images/pokeball-open.png'
 import pokeballClosed from '../../images/pokeball-closed.png'
 import Tada from 'react-reveal/Tada'
+import { useMutation, useQuery } from '@apollo/client';
+import { CATCH_POKEMON, UNCATCH_POKEMON } from '../../utils/mutations';
+import { QUERY_USER } from '../../utils/queries';
 
-export default function GenFour() {
 
-const [pokemonCaught, setPokemonCaughtStatus] = useState('uncaught');
-const [data, setData] = useState([]);
+export default function  GenFour() {
 
-function pokeballClickHandler(entry){
-  
-  if(pokemonCaughtArray.includes(entry)){
-    const index = pokemonCaughtArray.indexOf(entry)
-    pokemonCaughtArray.splice(index, 1)
-    document.getElementById(`pokeballImage${entry}`).src = pokeballOpen
-  }else{
-    pokemonCaughtArray.push(entry)
-    document.getElementById(`pokeballImage${entry}`).src = pokeballClosed
-  }
-  console.log(pokemonCaughtArray)
-}
+// fetching all pokemon from JSON Data
+const [JSONdata, setJSONData] = useState([]);
 
 const getData=()=>{
   fetch('./gen-4.json'
@@ -35,50 +25,123 @@ const getData=()=>{
       return response.json();
     })
     .then(function(myJson) {
-      setData(myJson)
+      setJSONData(myJson)
     })
 }
 useEffect(()=>{
   getData()
 },[])
+// end of fetch
+
+
+// username
+const currentUser = localStorage.getItem('username')
+
+
+// queries and mutations
+const [catchPokemon, { catchErr, catchData }] = useMutation(CATCH_POKEMON);
+const [unCatchPokemon, { unCatchErr, unCatchData }] = useMutation(UNCATCH_POKEMON);
+
+const { loading, data: userValue } = useQuery(QUERY_USER, {
+  variables: { username: currentUser },
+  
+});
+
+
+// users pokemon caught state
+const [userData, setUserData] = useState({
+  pokemonCaught: [],
+});
+
+
+//  setting the state of pokemon caught
+useEffect(() => {
+  if (!loading && userValue ) {
+    setUserData({ 
+      pokemonCaught: userValue?.user?.pokemonCaught?.map((caught) => caught.entry)
+    });
+  }
+}, [ loading, userValue ]);
+ 
+
+function newCatch(entry) {
+
+  catchPokemon({
+    variables: {
+      username: currentUser,
+      entry: parseFloat(entry)
+    }
+  });
+
+  window.location.reload(false);
+};
+
+
+function releaseCatch(entry) {
+
+    unCatchPokemon({
+      variables: {
+        username: currentUser,
+        entry: parseFloat(entry)
+      }
+    });
+
+    window.location.reload(false);
+};
 
   return (
     <div className='gen-four-bg'>
       <div className='container pt-3'>
         <ul className="list-group"> 
-        {data.map((element, i) => {
-          return (
-          <li className="list-group-item-success m-1" key={element.entry}>
+        { JSONdata.map((pokemon, i) => {
+          return (  
+          <li className="list-group-item-success m-1" key={pokemon.entry}>
             <ul className='d-flex justify-content-between align-items-center'>
               <div className='d-flex align-items-center justify-content-around'>
                 <li>
                   <img 
                     width='70px'
-                    src={`../../assets/sprites/Gen-4-Sprites/${element.entry}.png`}>
+                    src={`../../assets/sprites/Gen-4-Sprites/${pokemon.entry}.png`}
+                    >
                   </img>
                 </li>
                 <li className='m-4'>
-                  <h4>#{element.entry}</h4>
+                  <h4>#{pokemon.entry}</h4>
                 </li>
               </div>
-              <li><h4>{element.name}</h4></li>
-              <li><h6>{element.type}</h6></li> 
+              <li><h4>{pokemon.name}</h4></li>
+              <li><h6>{pokemon.type}</h6></li> 
               <li>
-              <Tada duration={2500}>   
+
+                <Tada duration={2500}> 
+              { !userData.pokemonCaught.includes(parseFloat(pokemon.entry)) 
+
+              ?
                 <img
-                  id={`pokeballImage${element.entry}`}
+                  key={pokemon.entry}
                   className='pokeball'
-                  src={pokemonCaughtArray.includes(element.entry) ? pokeballClosed : pokeballOpen}
-                  onClick={() => pokeballClickHandler(element.entry)}
+                  src={pokeballOpen}
+                  onClick={() => newCatch(pokemon.entry)}
                   >
                 </img>
-              </Tada>
+
+              :
+                <img
+                  key={pokemon.entry}
+                  className='pokeball'
+                  src={pokeballClosed}
+                  onClick={() => releaseCatch(pokemon.entry)}
+                  >
+                </img>
+              }
+
+                </Tada>
               </li>  
-            </ul>
-          </li>)
-        })}
+            </ul>    
+          </li>
+          )})}
         </ul>
-      </div>
+      </div>  
     </div>
   );
 }
