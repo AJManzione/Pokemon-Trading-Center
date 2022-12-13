096ce355320b322deec0d86c7f46fe7c04fd472f
michael seaman <mseaman26@gmail.com>
2022-12-12 17:33:21 -0800

    backend updateSprite mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,6 +5,7 @@ const typeDefs = gql`
     _id: ID
     username: String
     pokemonCaught: [Pokemon]
+    sprite: String
   }
 
   type Pokemon {
@@ -32,6 +33,7 @@ const typeDefs = gql`
     removeUser(username: String!): User
     catchPokemon(username: String!, entry: Int!): User
     unCatchPokemon(username: String!, entry: Int!): User
+    updateSprite(username: String!, sprite: String): User
   }
 `;
 module.exports = typeDefs;
